5bc4781ab9a8cb38c31235db9696128ca631b6c5
Sharon <168245948+Sharonanelisse@users.noreply.github.com>
2024-11-23 23:30:50 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Limonadas
+{
+    public bool DarCambio(int[] billetes)
+    {
+        int cinco = 0, diez = 0;
+
+        foreach (int billete in billetes)
+        {
+            if (billete == 5)
+            {
+                cinco++;
+            }
+            else if (billete == 10)
+            {
+                if (cinco == 0)
+                {
+                    return false;
+                }
+                cinco--;
+                diez++;
+            }
+            else if (billete == 20)
+            {
+                if (diez > 0 && cinco > 0)
+                {
+                    diez--;
+                    cinco--;
+                }
+                else if (cinco >= 3)
+                {
+                    cinco -= 3;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void Main(string[] args)
+    {
+        Limonadas puesto = new Limonadas();
+
+        Console.WriteLine(puesto.DarCambio(new int[] { 5, 5, 5, 10, 20 }));
+        Console.WriteLine(puesto.DarCambio(new int[] { 5, 5, 10, 10, 20 }));
+        Console.WriteLine(puesto.DarCambio(new int[] { 10, 10 }));
+        Console.WriteLine(puesto.DarCambio(new int[] { 5, 5, 10 }));
+    }
+}
